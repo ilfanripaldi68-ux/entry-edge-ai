513ee15f3baccbda02a6c9721c419b403cafcb74
ilfanripaldi68-ux <ilfanripaldi68@gmail.com>
2025-09-25 00:31:48 +0700

    Create ChartPage.tsx

---
+++ src/pages/ChartPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RealtimeChart from "../components/RealtimeChart";
+
+export default function ChartPage(){
+  return (
+    <div>
+      <h2>Realtime Chart Demo</h2>
+      <RealtimeChart />
+    </div>
+  );
+}
