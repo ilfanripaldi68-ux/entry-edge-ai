b5243df7e3a7bda6c6c37505d3e55b415395fa80
ilfanripaldi68-ux <ilfanripaldi68@gmail.com>
2025-09-25 23:13:54 +0700

    Create Footer.tsx

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="p-2 text-sm text-slate-400 text-center">
+      © Entry Edge AI
+    </footer>
+  );
+}
